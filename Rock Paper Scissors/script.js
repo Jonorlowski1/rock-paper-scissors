6eeab61fad40bde76ad8e8e91146d42472a50848
Jonathan <Jon.orlowski1@gmail.com>
2022-04-24 10:14:18 -0500

    Function that can match user to computer choice

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,7 +1,6 @@
 // !Pseudo-Code
 
-// Create function to randomize the choice for the computer and store it in a variable
-
+//? Push computer choice to empty array????
 // Create a loop that goes through the array?
 
 // Rock beats Scissors
@@ -11,6 +10,53 @@
 // clear user and comp
 // refresh the page
 
+
+
+const choices = ["rock", "paper", "scissors"];
+let rand = choices[Math.floor(Math.random() * choices.length)];
+let user = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
+
+// let comp = [];
+// function compChoice() {
+//   comp.push(rand);
+//   console.log(comp)
+// }
+// compChoice();
+
+const checkMatch = function (user, rand) {
+  console.log("working");
+  if (user === rand) {
+    console.log("match");
+  } else {
+    console.log("not a match");
+  }
+};
+
+checkMatch(user, rand);
+
+
+
+//! USER CHOICE IS STORED AND CHECKED
+// let userChoice = function () {
+//   for (let i = 0; i < choices.length; i++) {
+//     if (user === choices[i]) {
+//       return true;
+//     }
+//   }
+//   alert("Invalid Choice, please refresh and try again.")
+//   return false;
+// };
+// userChoice();
+
+// !MATCHING?
+// function userMatch(userChoice, choices) {
+//   if (userChoice === choices[i]) {
+//     return true
+//   } else {
+//     alert("does not match available choices")
+//   }
+// }
+// choices.forEach(userMatch);
 
 // * Winning/Losing conditions
 // if user === "rock" && comp === "scissors", return "you win",
@@ -24,33 +70,3 @@
 // if user === "scissors" && comp === "paper", return "you win",
 //  else if user === "scissors" && comp === "rock", return "you lose",
 //  else return draw
-
-const choices = ["rock", "paper", "scissors"];
-let user = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
-let rand = choices[Math.floor(Math.random() * choices.length)];
-
-// let userChoice = [];
-let compChoice = [];
-
-
-//! USER CHOICE IS STORED AND CHECKED
-let userChoice = function () {
-  for (let i = 0; i < choices.length; i++) {
-    if (user === choices[i]) {
-      return true;
-    }
-  }
-  alert("Invalid Choice, please refresh and try again.")
-  return false;
-};
-userChoice();
-
-// function userMatch(userChoice, choices) {
-//   if (userChoice === choices[i]) {
-//     return true
-//   } else {
-//     alert("does not match available choices")
-//   }
-// }
-
-// choices.forEach(userMatch);
