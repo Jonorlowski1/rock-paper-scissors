6c58ee22b09a473eb5d0ae5742327e69f0740d62
Jonathan <Jon.orlowski1@gmail.com>
2022-04-24 11:00:06 -0500

    Working fully, except nothing showing in console on return of win/lose

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,72 +1,46 @@
-// !Pseudo-Code
-
-//? Push computer choice to empty array????
-// Create a loop that goes through the array?
-
-// Rock beats Scissors
-// Scissors beats Paper
-// Paper beats Rock
-
-// clear user and comp
-// refresh the page
-
-
-
 const choices = ["rock", "paper", "scissors"];
-let rand = choices[Math.floor(Math.random() * choices.length)];
+let comp = choices[Math.floor(Math.random() * choices.length)];
 let user = prompt("Enter 'rock', 'paper', or 'scissors'").toLowerCase();
 
-// let comp = [];
-// function compChoice() {
-//   comp.push(rand);
-//   console.log(comp)
-// }
-// compChoice();
-
-const checkMatch = function (user, rand) {
-  console.log("working");
-  if (user === rand) {
-    console.log("match");
-  } else {
-    console.log("not a match");
+function userChoice() {
+  for (let i = 0; i < choices.length; i++) {
+    if (user === choices[i]) {
+      return true;
+    }
   }
-};
+  alert("Invalid Choice, please refresh and try again.")
+  // return false;
+  location.reload();
+}
+userChoice();
 
-checkMatch(user, rand);
-
-
-
-//! USER CHOICE IS STORED AND CHECKED
-// let userChoice = function () {
-//   for (let i = 0; i < choices.length; i++) {
-//     if (user === choices[i]) {
-//       return true;
-//     }
-//   }
-//   alert("Invalid Choice, please refresh and try again.")
-//   return false;
-// };
-// userChoice();
-
-// !MATCHING?
-// function userMatch(userChoice, choices) {
-//   if (userChoice === choices[i]) {
-//     return true
-//   } else {
-//     alert("does not match available choices")
-//   }
-// }
-// choices.forEach(userMatch);
-
-// * Winning/Losing conditions
-// if user === "rock" && comp === "scissors", return "you win",
-//  else if user === "rock" && comp === "paper", return "you lose",
-//  else return draw
-
-// if user === "paper" && comp === "rock", return "you win",
-//  else if user === "paper" && comp === "scissors", return "you lose",
-//  else return draw
-
-// if user === "scissors" && comp === "paper", return "you win",
-//  else if user === "scissors" && comp === "rock", return "you lose",
-//  else return draw
+function playGame(user, comp) {
+  if (user === "rock") {
+    if (comp === "paper") {
+      return "You Lose";
+    } else if (comp === "scissors") {
+      return "You Win";
+    } else {
+      return "It's a Draw";
+    }
+  }
+  if (user === "paper") {
+    if (comp === "scissors") {
+      return "You Lose";
+    } else if (comp === "rock") {
+      return "You Win";
+    } else {
+      return "It's a Draw";
+    }
+  }
+  if (user === "scissors") {
+    if (comp === "rock") {
+      return "You Lose";
+    } else if (comp === "paper") {
+      return "You Win";
+    } else {
+      return "It's a Draw";
+    }
+  }
+}
+playGame();
